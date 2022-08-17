9f224dc25d3ad0b8f9111cb8cc4245973091a367
adamsero <adamserodzinski@gmail.com>
2022-08-17 10:56:43 +0200

    Added a way to retrieve endpoint name for registration response packets

--- leshan-server-demo/src/main/java/org/eclipse/leshan/server/demo/servlet/statistics/ConnectionStatistics.java
+++ leshan-server-demo/src/main/java/org/eclipse/leshan/server/demo/servlet/statistics/ConnectionStatistics.java
@@ -3,16 +3,25 @@ package org.eclipse.leshan.server.demo.servlet.statistics;
 import org.eclipse.californium.core.coap.Message;
 import org.eclipse.californium.core.coap.Request;
 import org.eclipse.californium.core.coap.Response;
-import org.eclipse.leshan.server.demo.servlet.EventServlet;
 import org.eclipse.leshan.server.demo.servlet.log.CoapMessageListener;
 
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
 
 public class ConnectionStatistics {
     private enum MessageType {SEND_REQUEST, SEND_RESPONSE, RECEIVE_REQUEST, RECEIVE_RESPONSE}
 
+    private final Map<String, EndpointData> tokenEndpointMap = new ConcurrentHashMap<>();
     private final List<Map<String, String>> cachedStatistics = new ArrayList<>();
     private final StatisticsDataProxy statisticsDataProxy;
     private final Timer periodicFlushTimer;
@@ -24,6 +33,7 @@ public class ConnectionStatistics {
         periodicFlushTimer.scheduleAtFixedRate(new TimerTask() {
             @Override public void run() {
                 flushData();
+                clearOldTokenEndpointMapRecords();
             }
         }, periodBetweenFlushMs, periodBetweenFlushMs);
     }
@@ -33,11 +43,21 @@ public class ConnectionStatistics {
     }
 
     private synchronized void flushData() {
-        if(cachedStatistics.isEmpty()) {
+        if (cachedStatistics.isEmpty()) {
             return;
         }
         statisticsDataProxy.collectDataFromCache(cachedStatistics);
         cachedStatistics.clear();
+    }
+
+    private void clearOldTokenEndpointMapRecords() {
+        //If records sit in cache for more than an hour (adjustable), it's most likely a result
+        //of registration error, so they have to be deleted
+        long timeThreshold = 3_600_000;
+        Set<String> tokens = tokenEndpointMap.entrySet().stream()
+                .filter(entry -> System.currentTimeMillis() - entry.getValue().createdAt > timeThreshold)
+                .map(Map.Entry::getKey).collect(Collectors.toSet());
+        tokens.forEach(tokenEndpointMap::remove);
     }
 
     private void reportMessage(MessageType type, Message message, CoapMessageListener coapMessageListener) {
@@ -56,12 +76,19 @@ public class ConnectionStatistics {
         if (coapMessageListener != null) {
             messageData.put("endpoint", coapMessageListener.getEndpoint());
         } else {
-            //In this case we know it's registration request.
-            //So far I haven't found a way to retrieve endpoint name from registration response
-            String[] uriQueryList = message.getOptions().getUriQueryString().split("&");
-            Optional<String> endpointOptional = Arrays.stream(uriQueryList).filter(item -> item.startsWith("ep="))
-                    .findFirst();
-            endpointOptional.ifPresent(endpoint -> messageData.put("endpoint", endpoint.substring(3)));
+            //In this case we know it's registration request/response.
+            Optional<String> endpointOptional = message.getOptions().getUriQuery().stream()
+                    .filter(item -> item.startsWith("ep=")).findFirst();
+            if (endpointOptional.isPresent()) {
+                //Only requests carry endpoint name, so we cache it and wait for response report
+                String endpoint = endpointOptional.get().substring(3);
+                messageData.put("endpoint", endpoint);
+                tokenEndpointMap.put(message.getTokenString(), new EndpointData(endpoint, System.currentTimeMillis()));
+            } else {
+                //In case of response we retrieve endpoint name from cache and delete the cached record
+                messageData.put("endpoint", tokenEndpointMap.get(message.getTokenString()).endpoint);
+                tokenEndpointMap.remove(message.getTokenString());
+            }
         }
 
         putDataInCache(messageData);
@@ -81,5 +108,15 @@ public class ConnectionStatistics {
 
     public void reportReceiveResponse(Response response, CoapMessageListener coapMessageListener) {
         reportMessage(MessageType.RECEIVE_RESPONSE, response, coapMessageListener);
+    }
+
+    private static class EndpointData {
+        private final String endpoint;
+        private final long createdAt;
+
+        public EndpointData(String endpoint, long createdAt) {
+            this.endpoint = endpoint;
+            this.createdAt = createdAt;
+        }
     }
 }
